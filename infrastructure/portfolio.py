837f484ada527f7692011149fd23d6cd133d4603
kingianfong <fongkingian@hotmail.com>
2020-01-16 21:25:54 +0800

    Complete order management system

--- infrastructure/portfolio.py
+++ infrastructure/portfolio.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 from abc import ABC, abstractmethod
+from collections import deque
+from dataclasses import asdict
 
 from event import DataEvent, FillEvent, Order, Signal
 from event_queue import EventQueue
@@ -10,7 +12,7 @@ from execution_handler import BacktestExecutionHandler
 
 class Portfolio(ABC):
     @abstractmethod
-    def update_fill(self, fill_event):
+    def update_fill(self, fill):
         pass
 
     @abstractmethod
@@ -25,78 +27,93 @@ class BacktestPortfolio(Portfolio):
         self.execution_handler = execution_handler
         self.symbol_list = data_handler.symbol_list
 
-        self.all_positions_list = [pd.Series(0, index=self.symbol_list, name=queue.current_time)]  # list of series
-        self.all_holdings_list = [self.construct_initial_holdings(initial_capital)]  # list of series
-        self.all_orders_list = []
-        
+        self.current_orders_dict = {symbol: {} for symbol in self.symbol_list}
         self.current_positions = pd.Series(0, index=self.symbol_list, name=queue.current_time)
         self.current_holdings = self.construct_initial_holdings(initial_capital)  # series
-        self.current_orders_list = []
+
+        self.all_orders = pd.DataFrame()
+        self.all_positions_list = [pd.Series(0, index=self.symbol_list, name=queue.current_time)]  # list of series
+        self.all_holdings_list = [self.construct_initial_holdings(initial_capital)]  # list of series
+
+
+    @property
+    def current_orders(self):
+        return pd.DataFrame(self.current_orders_dict)
 
     @property
     def all_positions(self):
         return pd.DataFrame(self.all_positions_list)
-    
+
     @property
     def all_holdings(self):
         return pd.DataFrame(self.all_holdings_list)
 
-    @property
-    def all_orders(self):
-        return pd.DataFrame(self.all_orders_list)
-
-    @property
-    def current_orders(self):
-        return pd.DataFrame(self.current_orders_list)
-    
     def construct_initial_holdings(self, initial_capital):  # market values of positions
         holdings = {symbol: 0.0 for symbol in self.symbol_list}
         holdings["cash"] = initial_capital
         holdings["commission"] = 0.0
         holdings["total"] = initial_capital
         return pd.Series(holdings, name=self.queue.current_time)
-    
-    def update_positions(self, fill_event):
-        # maybe can split position and holding updates
+
+    def update_positions(self, fill):
         self.current_positions.name = self.queue.current_time
-        self.current_positions[fill_event.symbol] += fill_event.quantity
+        self.current_positions[fill.symbol] += fill.quantity
         self.all_positions_list.append(self.current_positions.copy())
 
-    def update_holdings(self, fill_event):
+    def update_holdings(self, fill):
         self.current_holdings.name = self.queue.current_time
-        self.current_holdings["cash"] -= fill_event.cost
-        self.current_holdings[fill_event.symbol] = self.current_positions[fill_event.symbol] * fill_event.cost
-        self.current_holdings["commission"] += fill_event.commission
+        self.current_holdings["cash"] -= fill.cost
+        self.current_holdings[fill.symbol] = self.current_positions[fill.symbol] * fill.cost
+        self.current_holdings["commission"] += fill.commission
         self.all_holdings_list.append(self.current_holdings.copy())
 
-    def update_fill(self, fill_event):
-        self.update_positions(fill_event)
-        self.update_holdings(fill_event)
-        
+    def update_order_fill(self, fill):
+        assert self.current_orders_dict[fill.symbol].get(fill.cost), f"No orders at price {fill.cost}"
+
+        while fill.quantity > self.current_orders_dict[fill.symbol][fill.cost][0]:
+            fill.quantity -= self.current_orders_dict[fill.symbol][fill.cost].pop()
+            assert self.current_orders_dict[fill.symbol][fill.cost], "Fill quantity > outstanding orders"
+        self.current_orders_dict[fill.symbol][fill.cost][0] -= fill.quantity
+
+        if not self.current_orders_dict[fill.symbol][fill.cost][0]:  # if first element in deque == 0:
+            self.current_orders_dict[fill.symbol][fill.cost].pop()  # remove that element
+            if not self.current_orders_dict[fill.symbol][fill.cost]:  # if deque empty:
+                del self.current_orders_dict[fill.symbol][fill.cost]  # remove deque
+
+    def update_fill(self, fill):
+        self.update_positions(fill)
+        self.update_holdings(fill)
+        self.update_order_fill(fill)
+
     def risk_check(self):
         return True
 
     def record_order(self, order, send_time):
-        self.all_orders_list.append(order.record(send_time))
+        self.all_orders.append(pd.Series(asdict(order), name=send_time))
         if order.order_type == "MKT":
             return
-        self.current_orders_list.append(order.record(send_time))
+
+        if self.current_orders_dict[order.symbol].get(order.price):
+            self.current_orders_dict[order.symbol][order.price].append(order.quantity)
+        else:
+            self.current_orders_dict[order.symbol][order.price] = deque([order.quantity])
 
     def send_order_from_signal(self, signal):
         latency_start = pd.Timestamp.utcnow()
         order_type = "MKT"  # currently MKT orders only
 
-        if signal.signal_type == "EXIT":
+        if signal.signal_type == "EXIT":  # saw this in the guide
             if self.current_positions[signal.symbol] < 0:
                 direction = "BUY"
             else:
                 direction = "SELL"
+            quantity = int(self.current_positions[signal.symbol] * signal.strength)
         else:
             direction = signal.signal_type
-            
-        quantity = int(10 * signal.strength)
+            quantity = int(10 * signal.strength)
+
         price = 0.0  # MKT order
-        
+
         if self.risk_check():
             order = Order(signal.symbol, signal.exchange, order_type, direction, quantity, price)
             send_time = self.queue.current_time + (pd.Timestamp.utcnow() - latency_start)
@@ -146,6 +163,8 @@ if __name__ == "__main__":
     port = BacktestPortfolio(eq, dh, eh, init_cap)
 
     def check_update_fill(port, sym):
+        temp = port.update_order_fill
+        port.update_order_fill = lambda x: None
         et = pd.Timestamp.utcnow()
         exchange, quantity, cost = "BitMEX", 10, 5000
         fe = FillEvent(et, sym, exchange, quantity, cost)
@@ -160,17 +179,35 @@ if __name__ == "__main__":
         print("positions:")
         print(p0)
         print(p1)
+        port.update_order_fill = temp
 
-    # check_update_fill(port, fe)
+    check_update_fill(port, sym)
 
     def check_send_order_from_signal(port, eq):
-        exchange, type, strength = "BitMEX", "BUY", 1.0
-        sig = Signal(sym, exchange, type, strength)
+        sig = Signal(sym, "BitMEX", "BUY", 1.0)
         port.send_order_from_signal(sig)
-        print(port.all_orders)
-        print(port.current_orders)
         event = eq.get()
         print(event)
 
     check_send_order_from_signal(port, eq)
 
+    def check_order_recording(port):
+        print(port.current_orders)
+        o0 = Order(sym, "BitMEX", "LMT", "SELL", 10, 7300)
+        o1 = Order(sym, "BitMEX", "LMT", "SELL", 15, 7300)
+        o2 = Order(sym, "BitMEX", "LMT", "SELL", 15, 7301)
+        port.record_order(o0, pd.Timestamp.utcnow())
+        port.record_order(o1, pd.Timestamp.utcnow())
+        port.record_order(o2, pd.Timestamp.utcnow())
+        print(port.current_orders)
+        f0 = FillEvent(pd.Timestamp.utcnow(), sym, "BitMEX", 5, 7300)
+        f1 = FillEvent(pd.Timestamp.utcnow(), sym, "BitMEX", 10, 7300)
+        f2 = FillEvent(pd.Timestamp.utcnow(), sym, "BitMEX", 10, 7300)
+        port.update_order_fill(f0)
+        print(port.current_orders)
+        port.update_order_fill(f1)
+        print(port.current_orders)
+        port.update_order_fill(f2)
+        print(port.current_orders)
+
+    check_order_recording(port)
